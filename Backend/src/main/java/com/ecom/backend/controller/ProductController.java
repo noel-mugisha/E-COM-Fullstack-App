0a15eb4c0e86301ed85130ba2a0ef68bbe1bb78d
Noel <noelmugisha332@gmail.com>
2025-08-01 22:13:22 +0200

    Solved CORS errors and solved bug from Frontend

--- Backend/src/main/java/com/ecom/backend/controller/ProductController.java
+++ Backend/src/main/java/com/ecom/backend/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.ecom.backend.controller;
 import com.ecom.backend.model.Product;
 import com.ecom.backend.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
+@CrossOrigin
 public class ProductController {
     private final ProductService productService;
 
