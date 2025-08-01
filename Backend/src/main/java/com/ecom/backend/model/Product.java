f684084d9903984e6734571bc747b216d8b26a21
Noel <noelmugisha332@gmail.com>
2025-08-01 21:29:02 +0200

    Added Product model in the backend

---
+++ Backend/src/main/java/com/ecom/backend/model/Product.java
@@ -0,0 +1,26 @@
+package com.ecom.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String brand;
+    private double price;
+    private String category;
+    private Date releaseDate;
+}
